fd09ff42f19ee62202eb5f099997a72f257ba1c3
liuyong <1032380143@qq.com>
2019-08-29 15:01:18 +0800

    添加登录布局

--- app/pages/login/index.js
+++ app/pages/login/index.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
-import {View,Text} from 'react-native';
-import {ListItem} from 'react-native-elements';
+import {View, ImageBackground,Alert} from 'react-native';
+import {Text, Input, Avatar,Button} from 'react-native-elements';
+
+import style from './style';
+
 
 export default class UserInfo extends Component {
+    state = {
+      user:'',
+      password:'',
+    };
+    onSubmit = () =>{
+          Alert.alert(JSON.stringify(this.state))
+    };
+
     render() {
+        const backgroundImageUri = require('../../assets/login.jpg');
+        const logo = require('../../assets/logo.jpg');
+        const {user,password} = this.state;
         return (
-            <View>
-                <Text>login</Text>
-            </View>
+            <ImageBackground source={backgroundImageUri} style={style.container}>
+                <View style={style.main}>
+                    <Text h4 style={style.title}>实现梦想</Text>
+                    <Avatar source={logo} rounded size="large" />
+                </View>
+                <View style={style.form}>
+                    <Input
+                        placeholder="请输入手机"
+                        inputStyle={{color:'#fff'}}
+                        containerStyle={{height:80}}
+                        placeholderTextColor="#fff"
+                        maxLength={20}
+                        defaultValue={user}
+                        // value={user}
+                    />
+                    <Input
+                        placeholder="请输入密码"
+                        inputStyle={{color:'#fff'}}
+                        containerStyle={{height:80}}
+                        placeholderTextColor="#fff"
+                        maxLength={50}
+                        defaultValue={password}
+                        // value={password}
+                    />
+                    <Button title="登录" onPress={this.onSubmit}/>
+                </View>
+            </ImageBackground>
         );
     }
 }
---
+++ app/pages/login/style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container:{
+        width:'100%',
+        height:'100%',
+        flex:1,
+    },
+
+    main:{
+        alignItems:'center',
+        marginTop:'15%',
+    },
+
+    title:{
+        height:46,
+        color:'#fff',
+    },
+
+    form:{
+        padding:30
+    }
+})
