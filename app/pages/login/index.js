cc9b033a16ead50bb82a5dc558d8adefa0a7eba0
liuyong <1032380143@qq.com>
2019-09-12 11:14:16 +0800

    perf:登录界面按钮添加loading

--- app/pages/login/index.js
+++ app/pages/login/index.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
-import {View, ImageBackground, Alert} from 'react-native';
-import {Text, Input, Avatar, Button} from 'react-native-elements';
-import {createForm} from 'rc-form';
+import React, { Component } from 'react';
+import { View, ImageBackground, Alert } from 'react-native';
+import { Text, Input, Avatar, Button } from 'react-native-elements';
+import { createForm } from 'rc-form';
 import request from '../../utils/request';
 
 import style from './style';
-import {Actions} from "react-native-router-flux";
+import { Actions } from "react-native-router-flux";
 
 
 class UserInfo extends Component {
@@ -13,15 +13,22 @@ class UserInfo extends Component {
         super(props);
         this.state = {
             user: '',
-            password: ''
+            password: '',
+            // 登录按钮
+            loading: false,
         }
     }
 
     onSubmit = () => {
-        const {validateFields} = this.props.form;
+        const { validateFields } = this.props.form;
         validateFields((err, value) => {
             if (!err) {
-                Alert.alert(JSON.stringify(value))
+                Alert.alert(JSON.stringify(value));
+                this.setState({ loading: true });
+
+                setTimeout(()=>{
+                    this.setState({ loading: false });
+                },2000)
                 // const result = request('http://192.168.0.25:3000/api/login',{
                 //     method:'POST',
                 //     body:value
@@ -47,8 +54,9 @@ class UserInfo extends Component {
     render() {
         const backgroundImageUri = require('../../assets/login.jpg');
         const logo = require('../../assets/logo.jpg');
-        const {user, password} = this.state;
-        const {getFieldProps, getFieldError, setFieldsValue} = this.props.form;
+        const { user, password, loading } = this.state;
+        const { getFieldProps, getFieldError, setFieldsValue } = this.props.form;
+        const loginButtonProps = loading ? { loading: true, disabled: true } : {};
         return (
             <ImageBackground source={backgroundImageUri} style={style.container}>
                 <View style={style.main}>
@@ -58,13 +66,13 @@ class UserInfo extends Component {
                 <View style={style.form}>
                     <Input
                         placeholder="请输入手机"
-                        inputStyle={{color: '#fff'}}
-                        containerStyle={{height: 70}}
+                        inputStyle={{ color: '#fff' }}
+                        containerStyle={{ height: 70 }}
                         placeholderTextColor="#DDE9FF"
                         maxLength={20}
                         errorMessage={getFieldError('user')}
                         onChangeText={(text) => {
-                            setFieldsValue({user: text})
+                            setFieldsValue({ user: text })
                         }}
                         {
                             ...getFieldProps('user', {
@@ -80,12 +88,12 @@ class UserInfo extends Component {
                     />
                     <Input
                         placeholder="请输入密码"
-                        inputStyle={{color: '#fff'}}
-                        containerStyle={{height: 70}}
+                        inputStyle={{ color: '#fff' }}
+                        containerStyle={{ height: 70 }}
                         placeholderTextColor="#DDE9FF"
                         maxLength={50}
                         onChangeText={(text) => {
-                            setFieldsValue({password: text})
+                            setFieldsValue({ password: text })
                         }}
                         errorMessage={getFieldError('password')}
                         secureTextEntry
@@ -102,10 +110,11 @@ class UserInfo extends Component {
                             })
                         }
                     />
-                    <Button title="登录" onPress={this.onSubmit}/>
-                    <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between", marginTop: 15}}>
-                        <Button title="忘记密码" type="clear" titleStyle={{color: '#FFF'}}/>
-                        <Button title="注册" type="clear" titleStyle={{color: '#FFF'}} onPress={()=>Actions.register()}/>
+                    <Button title="登录" {...loginButtonProps} onPress={this.onSubmit}/>
+                    <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between", marginTop: 15 }}>
+                        <Button title="忘记密码" type="clear" titleStyle={{ color: '#FFF' }}/>
+                        <Button title="注册" type="clear" titleStyle={{ color: '#FFF' }}
+                                onPress={() => Actions.register()}/>
                     </View>
                 </View>
             </ImageBackground>
