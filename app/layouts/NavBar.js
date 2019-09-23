bd7bdc1f1ac58a23d26c4e7f491ed7df1043b335
liuyong <1032380143@qq.com>
2019-09-23 10:45:23 +0800

    导航添加navBar

--- app/layouts/NavBar.js
+++ app/layouts/NavBar.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text, Alert, StatusBar, CameraRoll, PermissionsAndroid } from 'react-native';
 import {Icon} from 'react-native-elements';
+import {Actions} from 'react-native-router-flux';
 import NavBar from '../components/NavBar/Index';
 
 export default class Index extends Component {
+    componentDidMount() {
+        // console.log(Actions)
+    }
 
     render() {
+        const {currentParams:{title}} = Actions;
         return (
                 <NavBar
-                    leftComponent={{ icon: 'menu', color: '#fff' }}
-                    centerComponent={{ text: '首页', style: { color: '#fff' } }}
-                    rightComponent={{ icon: 'home', color: '#fff' }}
+                    leftComponent={{ icon: 'home', color: '#fff' }}
+                    centerComponent={{ text: title, style: { color: '#fff' } }}
+                    rightComponent={{ icon: 'menu', color: '#fff' }}
+                    statusBarProps={{backgroundColor:'#181818'}}
                     {...this.props}
                 />
         );
